77cd81dcbc30e4ccfb73426142740378a690b66f
‟ElaineNerea” <ela.amish@Gmail.com>
2017-10-31 11:38:50 +0300

    Add content t scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
-$(document).ready(function() {
-    $("form#ping-pong").submit(function(event) {
-      event.preventDefault();
-      var number = parseInt($("input#number").val());
-      var result = pingpong(number);
-      $("#result").text(result);
+$(document).ready(function () {
+    $("#number form").submit(function (event) {
+        event.preventDefault();
+        $("ul").empty();
+        var inputtedNumber = parseInt($("#inputNumber").val());
+        var countedNumbers = countUp(inputtedNumber);
+        for (var first = 0; first <= countedNumbers.length - 1; first++) {
+            $("ul#output").append("<li>" + countedNumbers[first] + "</li>");
+        };
+        clearForm("input");
     });
-  });
+});
+
+
+
+
+function countUp(number) {
+    var total = [];
+    for (var first = 1; first <= number; first++) {
+        if (first % 15 === 0) {
+            total.push("PINGPONG");
+        } else if (first % 5 === 0) {
+            total.push("PONG");
+        } else if (first % 3 === 0) {
+            total.push("PING");
+        } else {
+            total.push(first);
+        }
+    };
+    return total;
+};
