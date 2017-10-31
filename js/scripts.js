2c24f1cd4024d9d247fbf400935eb70affb6c3a6
‟ElaineNerea” <ela.amish@Gmail.com>
2017-10-31 11:47:17 +0300

    Add results section

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,22 @@
-$(document).ready(function () {
-    $("#number form").submit(function (event) {
-        event.preventDefault();
-        $("ul").empty();
-        var inputtedNumber = parseInt($("#inputNumber").val());
-        var countedNumbers = countUp(inputtedNumber);
-        for (var first = 0; first <= countedNumbers.length - 1; first++) {
-            $("ul#output").append("<li>" + countedNumbers[first] + "</li>");
-        };
-        clearForm("input");
+$(document).ready(function() {
+    $("form#ping-pong").submit(function(event) {
+      event.preventDefault();
+      var number = parseInt($("input#number").val());
+      var result = pingpong(number);
+      $("#result").text(result);
     });
-});
-
-
+  });
 
 
 function countUp(number) {
     var total = [];
     for (var first = 1; first <= number; first++) {
         if (first % 15 === 0) {
-            total.push("PINGPONG");
+            total.push("");
         } else if (first % 5 === 0) {
-            total.push("PONG");
+            total.push("");
         } else if (first % 3 === 0) {
-            total.push("PING");
+            total.push("");
         } else {
             total.push(first);
         }
